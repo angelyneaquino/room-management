17942b4e4bd6179f0f3733ef614d696036ea5f32
angelyneaquino <angelyneaquino10@gmail.com>
2025-04-18 20:42:36 +0800

    Added band panels and header panel

--- app.py
+++ app.py
@@ -6,7 +6,23 @@ window = Tk()
 window.title("Resistor Color Code")
 window.minsize(1000, 800)
 
-main_panel = Frame(window, bg="lightgray", width=1000, height=800)
-main_panel.place(relx=0.5, rely=0.5, anchor="center")
+# Main container to hold all the panels for each resistor band
+main_container = Frame(window, bg="lightgray", width=1000, height=800)
+main_container.place(relx=0.5, rely=0.5, anchor="center")
+
+# Header panel
+header_panel = Frame(main_container, bg="blue", width=1000, height=200)
+header_panel.place(x=0, y=0, anchor="nw")
+
+# Resistor band panels
+band4_panel = Frame(main_container, bg="red", width=1000, height=600)
+band4_panel.place(x=0, y=200, anchor="nw")
+band5_panel = Frame(main_container, bg="red", width=1000, height=600)
+band5_panel.place(x=0, y=200, anchor="nw")
+band6_panel = Frame(main_container, bg="red", width=1000, height=600)
+band6_panel.place(x=0, y=200, anchor="nw")
+
+
+
 
 window.mainloop()
